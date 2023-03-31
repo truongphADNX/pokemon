adb4f195e6d5d27d6ea04cf221ed7f06adb38d8a
phamtruong <truong.pham@adnx.vn>
2023-03-31 11:20:35 +0700

    fix bug

--- src/components/InteractScreen.vue
+++ src/components/InteractScreen.vue
@@ -4,8 +4,8 @@
       class="screen__inner"
       :style="{
         width: `${
-          ((((850 - 32 * 2) / Math.sqrt(cardsContext.length) - 10) * 3) / 4 +
-            10) *
+          ((((850 - 32 * 2) / Math.sqrt(cardsContext.length) - 16) * 3) / 4 +
+            16) *
           Math.sqrt(cardsContext.length)
         }px`,
       }"
